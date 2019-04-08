d531fe00e7a9e1a8d90cde85e6be216ce4f137fe
Mariya-Z <zubkovaa.mariya@yandex.ru>
2019-04-08 09:57:06 +0300

    first webinar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AppRoutingModule } from './app-routing.module';
+import { LayoutModule } from './layout/layout.module';
+import { TasksModule } from './tasks/tasks.module';
 
 import { AppComponent } from './app.component';
 
@@ -10,12 +15,19 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+
+    LayoutModule,
+    TasksModule,
+
+    AppRoutingModule,
   ],
   providers: [
   ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
-
+  constructor(router: Router) {
+    console.log('Routes: ', JSON.stringify(router.config, undefined, 2));
+  }
 }
