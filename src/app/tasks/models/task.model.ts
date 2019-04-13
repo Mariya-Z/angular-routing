51207b65c7fd513db5453cc67ac35b36af4648bf
Mariia Zubkova <mariia_zubkova@epam.com>
2019-04-13 21:41:03 +0300

    fix task

--- src/app/tasks/models/task.model.ts
+++ src/app/tasks/models/task.model.ts
@@ -1,13 +1,13 @@
 export class TaskModel {
-    constructor(
+  constructor(
     public id: number = null,
     public action: string = '',
     public priority: number = 0,
     public estHours: number = 0,
     public actHours?: number,
-    public done?: boolean
-    ) {
+    public done?: boolean,
+  ) {
     this.actHours = actHours || 0;
     this.done = done || false;
-    }
-    }
+  }
+}
--- src/app/tasks/services/task-array.service.ts
+++ src/app/tasks/services/task-array.service.ts
@@ -5,11 +5,11 @@ import { TasksServicesModule } from '../tasks-services.module';
 const taskList = [
   new TaskModel(1, 'Estimate', 1, 8, 8, true),
   new TaskModel(2, 'Create', 2, 8, 4, false),
-  new TaskModel(3, 'Deploy', 3, 8, 0, false)
+  new TaskModel(3, 'Deploy', 3, 8, 0, false),
 ];
 const taskListPromise = Promise.resolve(taskList);
 @Injectable({
-  providedIn: TasksServicesModule
+  providedIn: TasksServicesModule,
 })
 export class TaskArrayService {
   getTasks(): Promise<TaskModel[]> {
@@ -17,8 +17,8 @@ export class TaskArrayService {
   }
   getTask(id: number | string): Promise<TaskModel> {
     return this.getTasks()
-    .then(tasks => tasks.find(task => task.id === +id))
-    .catch(() => Promise.reject('Error in getTask method'));
+      .then(tasks => tasks.find(task => task.id === +id))
+      .catch(() => Promise.reject('Error in getTask method'));
   }
   createTask(task: TaskModel): void {
     taskList.push(task);
@@ -26,13 +26,13 @@ export class TaskArrayService {
   updateTask(task: TaskModel): void {
     const i = taskList.findIndex(t => t.id === task.id);
     if (i > -1) {
-    taskList.splice(i, 1, task);
-  }
+      taskList.splice(i, 1, task);
+    }
   }
   deleteTask(task: TaskModel): void {
     const i = taskList.findIndex(t => t.id === task.id);
     if (i > -1) {
-    taskList.splice(i, 1);
+      taskList.splice(i, 1);
     }
   }
 }
